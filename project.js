64a18b4841c09bdeb1b50bd25eef91ce3ab2ab33
carlosmsb <carlosmsb94@gmail.com>
2023-11-01 17:01:27 -0700

    updateCommit

--- project.js
+++ project.js
@@ -2,8 +2,26 @@
 //6. give the user their winnings
 //7. play again
 
-//1. Deposit money
 const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A:2,
+    B:4,
+    C:6,
+    D:8
+}
+
+const SYMBOL_VALUES = {
+    A:5,
+    B:4,
+    C:3,
+    D:2
+}
+
+//1. Deposit money
 
 const deposit = () => {
 while (true) {
@@ -42,6 +60,23 @@ const getBet = (balance, lines) => {
                 console.log("Invalid bet, try again.")
             } else {
                 return numberBet;
+            }
+        }
+};
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+        const reels = [[], [], []];
+        for (let i = 0; i < COLS; i++) {
+            const reelSymbols = [...symbols];
+            for (let j = 0; j < ROWS; j++) {
+                    const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+                const selectedSymbol = reelSymbols[]
             }
         }
 };
